7f07de38ec1c79830f7c49e89d6c6dd83b09c9ad
clement <clement@bird.eu>
2023-12-06 16:34:49 +0100

    feat: add getTypeInstance method to ProductObject

--- Model/Catalog/ProductObject.php
+++ Model/Catalog/ProductObject.php
@@ -14,4 +14,8 @@ use Magento\Catalog\Api\Data\ProductInterface;
 
 abstract class ProductObject implements ProductInterface, DataObjectInterface
 {
+    /**
+     * @return \Magento\Catalog\Model\Product\Type\AbstractType
+     */
+    abstract public function getTypeInstance() : \Magento\Catalog\Model\Product\Type\AbstractType;
 }
