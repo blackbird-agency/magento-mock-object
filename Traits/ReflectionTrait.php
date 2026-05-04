18fee0124d69c14b40e065f56504ef90669949f2
Sébastien Guérin <sebastien@bird.eu>
2026-05-04 15:00:03 +0200

    feat: adds ReflectionTrait

---
+++ Traits/ReflectionTrait.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Blackbird\MagentoMockObject\Traits;
+
+/**
+ * Provide useful methods with reflection.
+ *
+ * phpcs:ignoreFile
+ */
+trait ReflectionTrait
+{
+    /**
+     * @param object $object
+     * @param string $methodName
+     * @param array $parameters
+     *
+     * @return mixed
+     * @throws \ReflectionException
+     */
+    private function invokeMethod($object, $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
+    /**
+     * @param object $object
+     * @param string $propertyName
+     * @param mixed $value
+     * @param string $originalClassName
+     *
+     * @return object
+     * @throws \ReflectionException
+     */
+    private function setProperty($object, $propertyName, $value, $originalClassName = '')
+    {
+        $reflection = new \ReflectionClass($originalClassName ?: get_class($object));
+        $property = $reflection->getProperty($propertyName);
+        $property->setAccessible(true);
+        $property->setValue($object, $value);
+
+        return $object;
+    }
+
+    /**
+     * @param $object
+     * @param $propertyName
+     * @return object
+     * @throws \ReflectionExceptionœ
+     */
+    private function getProperty($object, $propertyName)
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $property = $reflection->getProperty($propertyName);
+        $property->setAccessible(true);
+        $property->getValue($object);
+
+        return $object;
+    }
+}
